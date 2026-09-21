0309edb2c589b8cb099c2d83688ce6c6b057c8f9
UnstableNicky <nickyvanschaijk@gmail.com>
2026-09-21 04:45:09 +0200

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+
+const CACHE='onze-dansreis-v6-1';
+const CORE=['./','./index.html','./base.css','./pwa.css','./course.js','./cloud.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./gift-splash.webp'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET')return;
+  const url=new URL(req.url);
+  if(req.mode==='navigate'){
+    event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res;}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  if(url.origin===location.origin){
+    event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res;})));
+    return;
+  }
+  if(url.hostname==='cdn.jsdelivr.net'){
+    event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res;})));
+  }
+});
